4f56ead3bc6e8a6396efddd475693322909c1c91
VargaDoro <varga.dorothea.timea@gmail.com>
2025-02-13 11:30:29 +0100

    ...

---
+++ adat.js
@@ -0,0 +1,20 @@
+export let kutyaLista = [
+    {
+      nev: "Morzsa",
+      nem: "kan",
+      kor: 12,
+      suly: 25,
+    },
+    {
+      nev: "Dézi",
+      nem: "szuka",
+      kor: 7,
+      suly: 20,
+    },
+    {
+      nev: "Drogo",
+      nem: "kan",
+      kor: 7,
+      suly: 35,
+    },
+  ];
--- main.js
+++ main.js
@@ -1,162 +1,24 @@
-//alert("Szép nap van!")
-console.log("szia");
+import { kutyaLista } from "./adat.js";
 
-/*adatszerkezetek
-vezérlőszerkezetek
+/*HTML - maniouláció
+HTML DOM - leképezi a htmlt egy documet object szerkezetbe
 */
 
-var szoveg1 = "első";
-let szoveg2 = "2";
-const szoveg3 = "haramdik";
+let koszonElem = document.getElementById("koszon");
+console.log(koszonElem);
+koszonElem.innerHTML = "szép az élet";
 
-/* const állandó, értéke nem változtatható meg */
+/* article elembe írd bele egy h2 es tagbe az első kutya nevét */
 
-let vmi = 2;
-
-console.log(vmi + 2);
-console.log(typeof vmi);
-console.log(typeof szoveg2);
-console.log(szoveg2 * 5);
-console.log(Number(szoveg2) + 2);
-
-let vaneJoKedvem = true;
-console.log(vaneJoKedvem);
-
-let a;
-console.log(2 * a);
-
-let lista = [];
-lista = [12, 34, 34, 12, 10];
-console.log(lista);
-console.log(typeof lista);
-
-if (lista[0] % 2 === 0) {
-  console.log("paros");
-} else {
-  console.log("páratlan");
-}
-
-let valt = "2";
-if (valt === 2) {
-  console.log("jó");
-} else {
-  console.log("nemjó");
-}
-
-let hetNapja = 2;
-switch (hetNapja) {
-  case 1:
-    console.log("hétfő");
-    break;
-  case 2:
-    console.log("kedd");
-    break;
-  case 3:
-    console.log("szerda");
-    break;
-  default:
-    console.log("ez nem nap");
-    break;
-}
-
-if (hetNapja === 1) {
-  console.log("hétfő");
-} else if (hetNapja === 2) {
-  console.log("kedd");
-} else {
-  console.log("nem nap");
-}
-
-/*ciklusok
-
-elöltesztelős: while*/
-
-let i = 0;
-while (i < lista.length) {
-  console.log(lista[i]);
-  i++;
-}
-
-for (let index = 0; index < lista.length; index++) {
-  console.log(lista[index]);
-}
-
-/*ojektumok osztályok nélkül*/
-
-let kutya1 = {
-  nev: "Morzsa",
-  nem: "kan",
-  kor: 12,
-  suly: 25,
-};
-
-let kutyaLista = [
-  {
-    nev: "Morzsa",
-    nem: "kan",
-    kor: 12,
-    suly: 25,
-  },
-  {
-    nev: "Dézi",
-    nem: "szuka",
-    kor: 7,
-    suly: 20,
-  },
-  {
-    nev: "Drogo",
-    nem: "kan",
-    kor: 7,
-    suly: 35,
-  },
-];
-
-console.log(kutyaLista[0].nev);
-
-/*
-0. hány kutyánk van?
-1. mennyi a kutyák átlag életkora?
-2. hány kan kutya van?
-3. legnagyobb súlyu kutya neve? */
-
-function feladat0(lista) {
-  return lista.length;
-}
-
-let db = feladat0(kutyaLista);
-
-console.log("0. feladat: ");
-console.log("A kutyák száma: " + db);
-console.log(`A kutyák száma: ${db}`);
-
-function feladat1(lista) {
-  let atlag = 0;
-  for (let index = 0; index < lista.length; index++) {
-    atlag += lista[index].kor;
-  }
-  let kora = atlag / lista.length;
-  return kora;
-}
-
-let atlagKor = feladat1(kutyaLista);
-
-console.log("1. feladat: ");
-console.log("A kutyák átlag életkora: " + atlagKor);
-
-function feladat2(lista) {
-  let kan = 0;
-  for (let index = 0; index < lista.length; index++) {
-    if (lista[index].nem === "kan") {
-      kan += 1;
-    }
-  }
-  return kan;
-}
-
-let kanKutya = feladat2(kutyaLista);
-console.log("2. feladat: ");
-console.log("A kan kutyákból ennyi van: " + kanKutya);
-
-function feladat3(lista){
+let kutyaNev = document.getElementById("art");
+let txt = "";
+for (let i = 0; i < kutyaLista.length; i++) {
+    txt += `<div class = "kutya">
+                        <h2>${kutyaLista[i].nev}</h2>
+                        <p>${kutyaLista[i].nem}</p>
+                        <p>${kutyaLista[i].kor}</p>
+                        <p>${kutyaLista[i].suly}</p>
+                    </div>`
     
 }
+kutyaNev.innerHTML = txt
